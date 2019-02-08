b9e6d7c254e9cf23e0cc8727ecac0166f72fa732
adam <bcuz@users.noreply.github.com>
2019-02-07 22:08:25 -0500

    more work on stretch

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,13 +1,19 @@
 class Tabs {
   constructor(element) {
-    // this.element = element
-    this.element = new TabLink(element)
+    this.element = element
+    this.tablink = new TabLink(this.element)
+    // console.log(this.tablink);
+    
+    // might need to be this.tablink
 
-    let parent = element.parentNode
-    
-    this.selected = parent.querySelector('.tabs-link-selected')
-    
-    
+    // call deselect from here.
+    this.element.addEventListener('click', () => this.deselect()) 
+  
+  }
+
+  deselect() {
+    // maybe pass in the currently selected tab?
+    this.tablink.deselect()
   }
 }
 
@@ -25,24 +31,35 @@ class TabLink {
     
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement)
+    // console.log(this.tabItem);
     
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener('click', () => this.select()) 
+    // this.element.addEventListener('click', () => this.deselect()) 
   };
 
   select() {
     
     // Get all of the elements with the tabs-link class
-    document.querySelectorAll('.tabs-link')
+    // document.querySelectorAll('.tabs-link')
 
-    // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    .forEach(link => link.classList.remove('tabs-link-selected'));
+    // // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+    // .forEach(link => link.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
     this.tabItem.select()
+  }
+
+  deselect() {
+    // console.log(this.element);
+    
+    let parent = this.element.parentNode
+    
+    let selected = parent.querySelector('.tabs-link-selected')
+    selected.classList.remove('tabs-link-selected');
   }
 }
 
